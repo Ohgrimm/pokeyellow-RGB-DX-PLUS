a9ad2ebbc50825cd2ddaa106c16c95e22791c953
Arthur Ferreira <arthur_f@hotmail.com>
2024-04-04 23:38:14 -0300

    Update names.asm
    Optimization for new matchups

--- data/types/names.asm
+++ data/types/names.asm
@@ -7,14 +7,14 @@ TypeNames:
 	dw .Poison
 	dw .Ground
 	dw .Rock
-	dw .Bird
 	dw .Bug
-	dw .Ghost
+	dw .Bird
 
 REPT UNUSED_TYPES_END - UNUSED_TYPES
 	dw .Normal
 ENDR
 
+	dw .Ghost
 	dw .Fire
 	dw .Water
 	dw .Grass
@@ -24,20 +24,19 @@ ENDR
 	dw .Dragon
 
 	assert_table_length NUM_TYPES
-
 .Normal:   db "NORMAL@"
 .Fighting: db "FIGHTING@"
 .Flying:   db "FLYING@"
 .Poison:   db "POISON@"
+.Ground:   db "GROUND@"
+.Rock:     db "ROCK@"
+.Bug:      db "BUG@"
+.Bird:     db "BIRD@"
+.Ghost:    db "GHOST@"
 .Fire:     db "FIRE@"
 .Water:    db "WATER@"
 .Grass:    db "GRASS@"
 .Electric: db "ELECTRIC@"
 .Psychic:  db "PSYCHIC@"
 .Ice:      db "ICE@"
-.Ground:   db "GROUND@"
-.Rock:     db "ROCK@"
-.Bird:     db "BIRD@"
-.Bug:      db "BUG@"
-.Ghost:    db "GHOST@"
 .Dragon:   db "DRAGON@"
